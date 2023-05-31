db1c23d93e83f01e9e1d5171c0259ea1a382bd79
Андрей Полушин <pandser@yandex.ru>
2023-05-31 17:18:59 +0300

    валидатор поля name модели Recipe

--- backend/recipes/validators.py
+++ backend/recipes/validators.py
@@ -1,9 +1,9 @@
-from re import fullmatch
+import re
 
 from django.core.exceptions import ValidationError
 
 
 def validate_name(value):
-    if not fullmatch(r'^[\w+.\-]$', value):
-        return ValidationError('Имя рецепта должно состоять из...')
+    if not re.fullmatch(r'^[а-яА-Яa-zA-Z\s]+$', value):
+        return ValidationError('Имя рецепта должно состоять из ')
     return value
